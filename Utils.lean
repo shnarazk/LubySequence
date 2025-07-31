c57e1526e2fd3b4d8c2adcd893d2f1cd88c63b91
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-07-31 15:10:58 +0900

    Add mod_eq_left theorem and complete mod_gt_right' proof

--- Utils.lean
+++ Utils.lean
@@ -39,13 +39,37 @@ theorem self_ne_pow_two_succ_of_size (n : Nat) : n < 2 ^ n.size.succ := by
   grind
 
 theorem mod_gt_right (a b : Nat) (h : 0 < b) : a % b < b := by exact Nat.mod_lt a h
+theorem mod_eq_left (a b : Nat) (ha : a < b) : a % b = a := by exact Nat.mod_eq_of_lt ha
 
 theorem mod_gt_right' (a b : Nat) (ha : 0 < a) (hb : 0 < b) :
     a % b = 0 → (a - 1) % b + 1 = b := by
   intro h
   simp [←Nat.dvd_iff_mod_eq_zero] at h
-  
-  sorry
+  have s1 : (a / b) * b = a := by exact Nat.div_mul_cancel h
+  have s2 : (a / b) * b = (a / b - 1) * b + b := by
+    calc
+      (a / b) * b = (a / b + 1 - 1) * b := by exact rfl
+      _ = (a / b - 1 + 1) * b := by
+        refine (Nat.mul_right_cancel_iff hb).mpr ?_
+        refine Nat.sub_add_comm ?_
+        have tf : a / b = 0 ∨ ¬(a / b = 0) := by exact eq_or_ne _ _
+        rcases tf with t|f
+        {
+          have a0 : a = 0 := by exact Nat.eq_zero_of_dvd_of_div_eq_zero h t
+          have : ¬ 0 < a := by exact Eq.not_gt a0
+          exact absurd ha this
+        }
+        {
+          have : 1 ≤ a / b := by exact Nat.one_le_iff_ne_zero.mpr f
+          exact this
+        }
+      _ = (a / b - 1) * b + 1 * b := by exact Nat.add_mul (a / b - 1) 1 b
+      _ = (a / b - 1) * b + b := by grind
+  rw [←s1, s2]
+  have : (a / b - 1) * b + b - 1 = (a / b - 1) * b + (b - 1) := by
+    exact Nat.add_sub_assoc hb ((a / b - 1) * b)
+  simp [this]
+  grind
 
 theorem mod_gt_right'' (a b : Nat) (ha : 0 < a) (hb : 0 < b) (h1 : a % b ≠ 0) :
     (a - 1) % b + 1 < b := by
