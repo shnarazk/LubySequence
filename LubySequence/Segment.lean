55768349dc6c22ace66aa3cd52ffebf084bee16e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-09-21 20:56:53 +0900

    Refactor segment to use n.size instead of (n + 2).size

--- LubySequence/Segment.lean
+++ LubySequence/Segment.lean
@@ -20,7 +20,7 @@ Return the segment index for `n`.
 def segment (n : ℕ) : ℕ := match n with
   | 0 => 1
   | n =>
-    let n' := 2 ^ ((n + 2).size - 2)
+    let n' := 2 ^ (n.size - 2)
     have n'_def : n' = value_of% n' := by exact rfl
     if h : n = 2 * n' - 2
     then n'
@@ -30,21 +30,20 @@ def segment (n : ℕ) : ℕ := match n with
         by_cases n_le_1 : n ≤ 1
         · have cases : n = 0 ∨ n = 1 := by exact le_one_iff_eq_zero_or_eq_one.mp n_le_1
           rcases cases with n_eq_0|n_eq_1
-          · simp [n_eq_0, size, binaryRec] at *
+          · simp [n_eq_0, size] at *
             simp [n'_def] at h
-          · simp [n_eq_1, size, binaryRec] at *
+          · simp [n_eq_1, size] at *
         · have n_ge_2 : n ≥ 2 := by
             have : n > 1 := by exact Nat.lt_of_not_le n_le_1
             exact this
-          have nsize2 : (n + 2).size ≥ 2 := by
-            have t1 : (n + 2).size ≥ (2 + 2).size := by
-              refine size_le_size ?_
-              · exact Nat.add_le_add_right n_ge_2 2
-            have t2 : (2 + 2).size = 3 := by simp [size, binaryRec]
+          have nsize2 : n.size ≥ 2 := by
+            have t1 : n.size ≥ (2 : ℕ).size := by
+              exact size_le_size n_ge_2
+            have t2 : (2 : ℕ).size = 2 := by simp [size, binaryRec]
             simp [t2] at t1
-            exact le_of_add_left_le t1
-          have : 2 * 2 ^ ((n + 2).size - 2) = 2 ^ ((n + 2).size - 1) := by
-            have : 2 * 2 ^ ((n + 2).size - 2) = 2 ^ ((n + 2).size - 2 + 1) := by
+            exact t1
+          have : 2 * 2 ^ (n.size - 2) = 2 ^ (n.size - 1) := by
+            have : 2 * 2 ^ (n.size - 2) = 2 ^ (n.size - 2 + 1) := by
               exact Eq.symm Nat.pow_succ'
             simp [this]
             rw [add_comm]
@@ -53,13 +52,10 @@ def segment (n : ℕ) : ℕ := match n with
           simp [this]
           refine Nat.sub_lt_right_of_lt_add ?_ ?_
           · exact le_pow (zero_lt_sub_of_lt nsize2)
-          · have t1 : 2 ^ ((n + 2).size - 1) ≤ n + 2 := by
-              exact n_ge_subenvelope (Nat.le_add_left 1 (n + 1))
-            have t2 : ¬2 ^ ((n + 2).size - 1) = n + 2 := by
-              by_contra envelope
-              have : n = 2 * n' - 2 := by simp [n'_def, this, envelope]
-              exact absurd this h
-            exact Nat.lt_of_le_of_ne t1 t2
+          · have t1 : 2 ^ (n.size - 1) ≤ n := by
+              exact n_ge_subenvelope (one_le_of_lt n_ge_2)
+            have t2 : 2 ^ (n.size - 1) < n + 1 := by exact Order.lt_add_one_iff.mpr t1
+            exact lt_succ_of_lt t2
       have decreasing : n - (2 * n' - 2) - 1 < n := by
         have : 2 * n' - 2 ≥ 0 := by exact Nat.zero_le (2 * n' - 2)
         replace : n - (2 * n' - 2) ≤ n := by exact sub_le n (2 * n' - 2)
@@ -79,7 +75,7 @@ theorem segment_prop1 : ∀ n : ℕ, n = 2 ^ n.size - 2 →
     segment (2 ^ (n.size + 1) - 2) = 2 * segment n := by
   intro n envelope
   rewrite (occs := .pos [2]) [segment]
-
+  sorry
   sorry
 
 
