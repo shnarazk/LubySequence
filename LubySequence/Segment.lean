b774e4173b4ca7219cc1b7c020dee37ef9f870c1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-09-21 17:15:56 +0900

    Refactor segment proof to use Nat.sub_lt_right_of_lt_add

--- LubySequence/Segment.lean
+++ LubySequence/Segment.lean
@@ -51,14 +51,15 @@ def segment (n : ℕ) : ℕ := match n with
             refine Eq.symm ((fun {b a c} h ↦ (Nat.sub_eq_iff_eq_add' h).mp) ?_ rfl)
             · exact le_sub_one_of_lt nsize2
           simp [this]
-          replace :(n + 2).size = n.size := by sorry
-          simp [this]
-          replace : 2 ^ (n.size - 1) < n := by
-            have : 2 ^ (n.size - 1) ≤ n := by
-              refine n_ge_subenvelope ?_
-              · sorry
-            sorry
-          exact sub_lt_of_lt this
+          refine Nat.sub_lt_right_of_lt_add ?_ ?_
+          · exact le_pow (zero_lt_sub_of_lt nsize2)
+          · have t1 : 2 ^ ((n + 2).size - 1) ≤ n + 2 := by
+              exact n_ge_subenvelope (Nat.le_add_left 1 (n + 1))
+            have t2 : ¬2 ^ ((n + 2).size - 1) = n + 2 := by
+              by_contra envelope
+              have : n = 2 * n' - 2 := by simp [n'_def, this, envelope]
+              exact absurd this h
+            exact Nat.lt_of_le_of_ne t1 t2
       have decreasing : n - (2 * n' - 2) - 1 < n := by
         have : 2 * n' - 2 ≥ 0 := by exact Nat.zero_le (2 * n' - 2)
         replace : n - (2 * n' - 2) ≤ n := by exact sub_le n (2 * n' - 2)
