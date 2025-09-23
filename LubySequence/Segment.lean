c1647c7fe59a3855f7c00da71cfebec929faad30
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-09-23 13:32:23 +0900

    Refactor segment_prop1 proof and clean up comments

--- LubySequence/Segment.lean
+++ LubySequence/Segment.lean
@@ -65,12 +65,12 @@ theorem segment_prop1 : ∀ n > 0, n = 2 ^ n.size - 2 →
   have n2size3 : (n + 2).size ≥ 3 := by
     have : n + 2 ≥ 2 + 2 := by exact Nat.add_le_add_right n_ge_2 2
     replace : (n + 2).size ≥ (2 + 2).size := by exact size_le_size this
-    have s2 : (2 + 2).size = 3 := by simp [size, binaryRec]
-    simp [s2] at this
+    have s1 : (2 + 2).size = 3 := by simp [size, binaryRec]
+    simp [s1] at this
     exact this
   let n' := 2 ^ (n + 2).size - 2
   have n'_def : n' = value_of% n' := by exact rfl
-  have n'_is_envelope : n' = 2 ^n'.size - 2 := by
+  have n'_is_envelope : n' = 2 ^ n'.size - 2 := by
     simp [n'_def]
     have : (2 ^ (n + 2).size - 2).size = (n + 2).size := by
       refine size_sub ?_ ?_ ?_
@@ -80,48 +80,40 @@ theorem segment_prop1 : ∀ n > 0, n = 2 ^ n.size - 2 →
         · exact Nat.sub_ne_zero_iff_lt.mpr (lt_of_add_left_lt n2size3)
     simp [this]
   rw [segment]
-  -- have n1 : n = 2 * 2 ^ (n.size - 2) - 2 := by sorry
-  sorry
-/-
-  rewrite (occs := .pos [1]) [segment]
-  · rewrite (occs := .pos [2]) [segment]
+  · split
+    · expose_names
+      have s1 : (2 ^ (n + 2).size - 2 + 2) = 2 ^ (n + 2).size := by
+        refine Nat.sub_add_cancel ?_
+        · exact Nat.le_self_pow (Nat.ne_zero_of_lt n2size3) 2
+      simp [s1]
+      replace s1 : (2 ^ (n + 2).size).size = (n + 2).size + 1 := by
+        exact size_pow
+      simp [s1]
+      rw [segment]
+      · split
+        · expose_names
+          have : 2 * 2 ^ ((n + 2).size - 2) = 2 ^ ((n + 2).size - 2 + 1) := by
+            exact Eq.symm Nat.pow_succ'
+          simp [this]
+          grind
+        · expose_names
+          simp
+          --
+          sorry
+      · intro n_eq_0
+        have : ¬n = 0 := by exact Nat.ne_zero_of_lt n_gt_0
+        exact absurd n_eq_0 this
     · expose_names
       simp
-      split
-      · expose_names
-        have : (2 ^ (n.size + 1) - 2).size = n.size + 1 := by
-          refine size_sub ?_ ?_ ?_
-          · exact zero_lt_succ n.size
-          · exact Nat.zero_lt_two
-          · simp
-            refine le_pow ?_
-            exact size_pos.mpr n_gt_0
-        simp [this]
-        replace : 2 * 2 ^ (n.size - 2) = 2 ^ (n.size - 1) := by
-          replace t1 : 2 * 2 ^ (n.size - 2) = 2 ^ (n.size - 2 + 1) := by
-            exact Eq.symm Nat.pow_succ'
-          replace t2 : n.size - 2 + 1 = n.size - 1 := by grind
-          simp [t2] at t1
-          exact t1
-        simp [this]
-        sorry
-      · expose_names
-        --
-        sorry
-    · expose_names
-      intro x
-      simp [x] at *
+      simp [←n'_def] at h
+      --
+      sorry
   · intro x
-    -- have t1 : n ≥ 1 := by exact n_gt_0
-    have t1 : n.size ≥ (1 : ℕ).size := by exact size_le_size n_gt_0
-    have : (1 : ℕ).size = 1 := by exact size_one
-    simp [this] at t1
-    replace : n.size + 1 ≥ 2 := by exact le_add_of_sub_le t1
-    replace : 2 ^ (n.size + 1) ≥ 2 ^ 2 := by exact Luby.pow2_le_pow2 2 (n.size + 1) this
-    simp at this
-    have c : 4 - 2 ≤ 2 ^ (n.size + 1) - 2 := by
-      exact Nat.sub_le_sub_right this 2
-    simp [x] at c
--/
+    have : 2 ^ (n + 2).size ≥ 2 ^ 3 := by
+      exact Luby.pow2_le_pow2 3 (n + 2).size n2size3
+    replace : 2 ^ (n + 2).size ≥ 8 := by exact this
+    replace : ¬2 ^ (n + 2).size - 2 = 0 := by
+      exact Nat.sub_ne_zero_iff_lt.mpr (lt_of_add_left_lt this)
+    exact absurd x this
 
 end LubySegment
