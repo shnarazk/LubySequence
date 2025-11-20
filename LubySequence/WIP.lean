e3e1295e8ac12f82812bc5e1806377c67ea986b4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-11-20 11:09:59 +0900

    Make imports public and add module headers

--- LubySequence/WIP.lean
+++ LubySequence/WIP.lean
@@ -1,12 +1,14 @@
+module
+
 import Mathlib.Tactic
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Nat.Init
 import Mathlib.Data.Nat.Bits
 import Mathlib.Data.Nat.Size
-import LubySequence.Utils
-import LubySequence.Basic
-import LubySequence.Segment
-import LubySequence.State
+public import LubySequence.Utils
+public import LubySequence.Basic
+public import LubySequence.Segment
+public import LubySequence.State
 
 namespace LubyState
 
