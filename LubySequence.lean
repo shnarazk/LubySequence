e89e99e0e3414b61bd4415924f482cc228502300
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-02-15 16:26:03 +0900

    remove Segment

--- LubySequence.lean
+++ LubySequence.lean
@@ -4,7 +4,6 @@ module
 -- Import modules here that should be built as part of the library.
 public import LubySequence.Utils
 public import LubySequence.Basic
-public import LubySequence.Segment
 public import LubySequence.SegmentSequence
 public import LubySequence.Equivalence
 public import LubySequence.WIP
